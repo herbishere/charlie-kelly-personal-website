a9d5c02aac65521587a25f67fed67cc8087dc05f
Herb <herbishere@users.noreply.github.com>
2019-05-21 22:13:05 -0500

    Split style.css, formatted home.html for carousel

---
+++ scripts/carousel.js
@@ -0,0 +1,4 @@
+var list = document.getElementById("carousel-images-list");
+var listMembers = document.getElementsByClassName("carousel-images-list-member");
+var navigation = document.getElementById("carousel-navigation");
+var navButtons = document.getElementsByClassName("carousel-button");
